c8072d3f553b39ca5c37a8802c43f9b236e47fca
Robert Rapiteanu <robert.rapiteanu@gmail.com>
2019-08-18 23:30:07 +0300

    fix error

--- config/prod.exs
+++ config/prod.exs
@@ -4,12 +4,4 @@ use Mix.Config
 config :logger, level: :info
 
 config :infusion, InfusionWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
-  check_origin: ["https://infusion.rrapiteanu.now.sh"]
-
-config :infusion, Infusion.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  database: "",
-  ssl: true,
-  pool_size: 2
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80]
